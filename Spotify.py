dadd832298f4624e9d434c12d7e1ce2115cfc315
sa1K <svsvsk@gmail.com>
2021-05-31 18:20:09 -0400

    added spotipy initialization

--- Spotify.py
+++ Spotify.py
@@ -1,2 +1,10 @@
 import spotipy
 import SpotAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+
+cid = SpotAuth.clientID
+secret = SpotAuth.clientSecret
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager
+                     =
+                     client_credentials_manager)
